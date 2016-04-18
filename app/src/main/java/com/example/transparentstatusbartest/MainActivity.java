e6c949374501fa72e5b35b6fca149a7fabaf1c9e
Gautam Jain <gautamjain@bankofamerica.com>
2016-04-18 16:01:55 -0700

    Add example activity with transparent status bar
    Status bar in example still uses colorPrimaryDark value despite statusBarColor being set to transparent.

---
+++ app/src/main/java/com/example/transparentstatusbartest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.transparentstatusbartest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setFullscreen();
+        setContentView(R.layout.activity_main);
+    }
+
+    private void setFullscreen() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+}
