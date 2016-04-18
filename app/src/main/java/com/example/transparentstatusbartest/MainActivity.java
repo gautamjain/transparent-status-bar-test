6c8ea056d843a5b4176296ff260f91fd05a60d99
Gautam Jain <gautamjain@bankofamerica.com>
2016-04-18 16:09:17 -0700

    Set status bar background color to transparent
    Setting the status bar color in Java works as expected.

--- app/src/main/java/com/example/transparentstatusbartest/MainActivity.java
+++ app/src/main/java/com/example/transparentstatusbartest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.transparentstatusbartest;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -12,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setFullscreen();
         setContentView(R.layout.activity_main);
+
+        // Set status bar to transparent.  This works as expected.
+        CoordinatorLayout container = (CoordinatorLayout) findViewById(R.id.container);
+        container.setStatusBarBackgroundColor(getResources().getColor(R.color.transparent));
     }
 
     private void setFullscreen() {
